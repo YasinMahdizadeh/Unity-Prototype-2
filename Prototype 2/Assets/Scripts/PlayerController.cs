06f8078d3d4e75006c5dddce48d46134ca8aaeb6
Yasin Mahdizadeh <yasin.mahdizade@gmail.com>
2020-05-14 15:37:09 +0430

    All Files Added

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xrange = 10f;
+    public GameObject projectilePrefab;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Fired");
+            Instantiate(projectilePrefab, transform.position, transform.rotation);
+        }
+        if (transform.position.x < -xrange)
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        
+        if (transform.position.x > xrange)
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float xrange = 20f;
+    private float firstDelay = 2f;
+    private float delayInterval = 2.2f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimals", firstDelay, delayInterval);
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimals();
+        }
+    }
+
+    void SpawnRandomAnimals()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Vector3 spawnPosition = new Vector3(Random.Range(-xrange, xrange), 0, 20);
+
+        Instantiate(animalPrefabs[animalIndex],
+            spawnPosition,
+            animalPrefabs[animalIndex].transform.rotation);
+    }
+}
